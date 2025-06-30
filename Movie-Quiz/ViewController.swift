c64efaff419fb6e08b6c4fec4494c4c4c087d5f5
Practicum <practicum@ya.ru>
2025-06-30 21:36:10 +0300

    commit

--- Movie-Quiz/ViewController.swift
+++ Movie-Quiz/ViewController.swift
@@ -8,86 +8,83 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var buttonYNo: UIButton!
-
+    
     @IBOutlet weak var buttonYes: UIButton!
     @IBOutlet weak var вопрос: UILabel!
- 
+    
     @IBOutlet weak var count: UILabel!
     @IBOutlet weak var imageView: UIImageView!
-    private var font = UIFont(name: "YSDisplay-Medium", size: 20)
+    
+    private var font = UIFont(name: "YSDisplay-Medium", size: 25)
+    
     @IBOutlet weak var рейтинг: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        вопрос.font = font
-        count.font = font
-        рейтинг.font = font
-        buttonYes.layer.cornerRadius = 12
-        buttonYNo.layer.cornerRadius = 12
-       
+        buttonYes.layer.cornerRadius = 15
+        buttonYNo.layer.cornerRadius = 15
+        if let font = font {
+            вопрос?.font = font
+            рейтинг?.font = font
+            count?.font = font
+            
+        }
     }
-   
-    private var array: [UIImage] = [UIImage(named: "The room")!, UIImage(named: "the lord of the rings the fellelo of the ring")!]//, UIImage(named: "Kill Bill")!, UIImage(named: "The Avengers")!, UIImage(named: "Inception")!, UIImage(named: "Mad Max: Fury Road")!, UIImage(named: "The Lion King")!, UIImage(named: "Pulp Fiction")!]
-    private var index = 0
-    var a = false
-    @IBAction func buttonNo(_ sender: Any) {
-        a = false
+        
+        
+        
+        private let index = 0
+        private let questions: [QuizQuestion] = [
+            QuizQuestion(
+                image: "The Godfather",
+                text: "Рейтинг этого фильма больше чем 6?",
+                correctAnswer: true),
+            QuizQuestion(
+                image: "The Dark Knight",
+                text: "Рейтинг этого фильма больше чем 6?",
+                correctAnswer: false),
+            QuizQuestion(
+                image: "Kill Bill",
+                text: "Рейтинг этого фильма больше чем 6?",
+                correctAnswer: true),
+            QuizQuestion(
+                image: "The Avengers",
+                text: "Рейтинг этого фильма больше чем 6?",
+                correctAnswer: true),
+            QuizQuestion(
+                image: "Deadpool",
+                text: "Рейтинг этого фильма больше чем 6?",
+                correctAnswer: false),
+            QuizQuestion(
+                image: "The Green Knight",
+                text: "Рейтинг этого фильма больше чем 6?",
+                correctAnswer: true),
+            QuizQuestion(
+                image: "Old",
+                text: "Рейтинг этого фильма больше чем 6?",
+                correctAnswer: false),
+            QuizQuestion(
+                image: "The Ice Age Adventures of Buck Wild",
+                text: "Рейтинг этого фильма больше чем 6?",
+                correctAnswer: false),
+            QuizQuestion(
+                image: "Tesla",
+                text: "Рейтинг этого фильма больше чем 6?",
+                correctAnswer: false),
+            QuizQuestion(
+                image: "Vivarium",
+                text: "Рейтинг этого фильма больше чем 6?",
+                correctAnswer: false)
+        ]
+        private func counter(model: QuizQuestion) -> QuizStepViewModel {
+            let a = QuizStepViewModel(
+                image: UIImage(
+                    named: model.image) ?? UIImage(),
+                question: model.text,
+                questionNumber: "\(index + 1)\(questions.count)")
+            return a
+        }
+        
+        
     }
-    @IBAction func buttonYes(_ sender: Any) {
-       a = true
-       // UIImage(named: array[0])
-    }
-    private let questions: [QuizQuestion] = [
-        QuizQuestion(
-            image: "The Godfather",
-            text: "Рейтинг этого фильма больше чем 6?",
-            correctAnswer: true),
-        QuizQuestion(
-            image: "The Dark Knight",
-            text: "Рейтинг этого фильма больше чем 6?",
-            correctAnswer: false),
-        QuizQuestion(
-            image: "Kill Bill",
-            text: "Рейтинг этого фильма больше чем 6?",
-            correctAnswer: true),
-        QuizQuestion(
-            image: "The Avengers",
-            text: "Рейтинг этого фильма больше чем 6?",
-            correctAnswer: true),
-        QuizQuestion(
-            image: "Deadpool",
-            text: "Рейтинг этого фильма больше чем 6?",
-            correctAnswer: false),
-        QuizQuestion(
-            image: "The Green Knight",
-            text: "Рейтинг этого фильма больше чем 6?",
-            correctAnswer: true),
-        QuizQuestion(
-            image: "Old",
-            text: "Рейтинг этого фильма больше чем 6?",
-            correctAnswer: false),
-        QuizQuestion(
-            image: "The Ice Age Adventures of Buck Wild",
-            text: "Рейтинг этого фильма больше чем 6?",
-            correctAnswer: false),
-        QuizQuestion(
-            image: "Tesla",
-            text: "Рейтинг этого фильма больше чем 6?",
-            correctAnswer: false),
-        QuizQuestion(
-            image: "Vivarium",
-            text: "Рейтинг этого фильма больше чем 6?",
-            correctAnswer: false)
-    ]
-    private func counter(model: QuizQuestion) -> QuizStepViewModel {
-        let a = QuizStepViewModel(
-            image: UIImage(
-                named: model.image) ?? UIImage(),
-            question: model.text,
-            questionNumber: "\(index + 1)\(questions.count)")
-        return a
-    }
-   
-    }
-  
