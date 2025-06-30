b3b5fece796f6e98e71b4c89355c2aaf5c4cef13
Practicum <practicum@ya.ru>
2025-06-30 15:31:32 +0300

    Настроил экраны начал работать во вью контроллере

--- Movie-Quiz/ViewController.swift
+++ Movie-Quiz/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var count: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    private var questions: [] = []
 
+    @IBAction func buttonNo(_ sender: Any) {
+   
+    }
+    @IBAction func buttonYes(_ sender: Any) {
+        
+    }
 
 }
 
